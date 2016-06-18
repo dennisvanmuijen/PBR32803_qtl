f0d2d2cdf3d71a50adc1b40dcb6dc4453de3cdae
dmu <d.van.muijen@rijkzwaan.nl>
2016-06-18 11:29:25 +0200

    test

--- server.R
+++ server.R
@@ -1,7 +1,6 @@
 library(shiny)
 shinyServer(function(input, output, session) {
-  
-  ##################################
+    ##################################
   ###### Upload data
   ##################################
   output$poptype <- renderUI({
