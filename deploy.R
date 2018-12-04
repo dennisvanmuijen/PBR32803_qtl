48561b927a9cad2a841b9501475af3606ab4d0d4
D. van Muijen <dmuijen@users.noreply.github.com>
2018-12-04 19:50:47 +0100

    Delete deploy.R

--- deploy.R
+++
@@ -1,7 +0,0 @@
-library(rsconnect)
-
-rsconnect::setAccountInfo(name='plantbreeding', 
-                          token='6B041505BB404821E433D65CBB835BE9',
-                          secret='eYYUzbJU/vHuEfzn/NM9CKQ/WI77e5FG9bRduVO6')
-deployApp(account = 'plantbreeding', appName = "PBR_32803_qtl")
-
